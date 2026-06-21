60069dfce92f01187e5ab8641d088eb3de1f47a8
sunliguo <sunliguo2006@qq.com>
2026-06-21 09:31:33 +0800

    更新 wireless_meter.ino 开头注释

--- wireless_meter.ino
+++ wireless_meter.ino
@@ -1,47 +1,23 @@
-/* *****************************************************************
+/*
+ * wireless_meter.ino
+ * 48V 新能源电动车监测板
  *
- * Download latest Blinker library here:
+ * 使用 Blinker 进行 APP 与云端数据交互。
+ * 需要 ESP8266/ESP32 Arduino 支持包以及 Blinker 库。
+ *
+ * Blinker 库:
  * https://github.com/blinker-iot/blinker-library/archive/master.zip
  *
- *
- * Blinker is a cross-hardware, cross-platform solution for the IoT.
- * It provides APP, device and server support,
- * and uses public cloud services for data transmission and storage.
- * It can be used in smart home, data monitoring and other fields
- * to help users build Internet of Things projects better and faster.
- *
- * Make sure installed 2.5.0 or later ESP8266/Arduino package,
- * if use ESP8266 with Blinker.
+ * ESP8266 Arduino >= 2.5.0:
  * https://github.com/esp8266/Arduino/releases
  *
- * Make sure installed 1.0.2 or later ESP32/Arduino package,
- * if use ESP32 with Blinker.
+ * ESP32 Arduino >= 1.0.2:
  * https://github.com/espressif/arduino-esp32/releases
  *
- * Docs: https://doc.blinker.app/
- *       https://github.com/blinker-iot/blinker-doc/wiki
- *
- * *****************************************************************
- *
- * Blinker 库下载地址:
- * https://github.com/blinker-iot/blinker-library/archive/master.zip
- *
- * Blinker 是一套跨硬件、跨平台的物联网解决方案，提供APP端、设备端、
- * 服务器端支持，使用公有云服务进行数据传输存储。可用于智能家居、
- * 数据监测等领域，可以帮助用户更好更快地搭建物联网项目。
- *
- * 如果使用 ESP8266 接入 Blinker,
- * 请确保安装了 2.5.0 或更新的 ESP8266/Arduino 支持包。
- * https://github.com/esp8266/Arduino/releases
- *
- * 如果使用 ESP32 接入 Blinker,
- * 请确保安装了 1.0.2 或更新的 ESP32/Arduino 支持包。
- * https://github.com/espressif/arduino-esp32/releases
- *
- * 文档: https://doc.blinker.app/
- *       https://github.com/blinker-iot/blinker-doc/wiki
- *
- * *****************************************************************/
+ * 文档:
+ * https://doc.blinker.app/
+ * https://github.com/blinker-iot/blinker-doc/wiki
+ */
 
 #define BLINKER_WIFI
 #define BLINKER_ALIGENIE_SENSOR
